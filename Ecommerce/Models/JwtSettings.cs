3ae4a2c8e94989d4659d8f5ddd835c68d0320fc8
sudemzehracelikell <s.zehracelikel@gmail.com>
2025-08-14 11:04:15 +0300

    update added

--- Ecommerce/Models/JwtSettings.cs
+++ Ecommerce/Models/JwtSettings.cs
@@ -7,8 +7,5 @@ public class JwtSettings
         public string Issuer { get; set; } 
         public string Audience { get; set; }
         
-        public int DeadLine { get; set; }
-        
-        //
-        
+        public int DeadLine { get; set; } 
 }
